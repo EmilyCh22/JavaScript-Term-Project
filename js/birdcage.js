fbfb14ac5278ab43d7ad9b76ba3e98ff71d2e473
Emily Cherkasova <cherka3@mail.nmc.edu>
2025-04-28 14:35:37 -0400

    Milestone 3
    - Finished game 2 (Word Search)
    - Made game 3 (Birdcage)
    - updated readMe.txt file
    - combined all CSS into one games.css file

--- js/birdcage.js
+++ js/birdcage.js
@@ -1,3 +1,131 @@
 document.getElementById("goHome").addEventListener("click", function() {
   window.location.href = "https://lisabalbach.com/cherka3/CIT190/FinalProject/gameHomepage.html"
 })
+
+const wordList = [
+  "Albatross", "Avocet", "BaldEagle", "BarnOwl", "BlueJay", "Bluebird", "Booby", "Canary", "Cardinal", "Chickadee",
+  "Cockatoo", "Cormorant", "Crane", "Crow", "Cuckoo", "Dove", "Duck", "Eagle", "Egret", "Emu",
+  "Falcon", "Finch", "Flamingo", "Goldfinch", "Goose", "Goshawk", "Grebe", "Grosbeak", "Gull", "Hawk",
+  "Heron", "Hoopoe", "Hummingbird", "Ibis", "Jay", "Junco", "Kestrel", "Kingfisher", "Kite", "Lark",
+  "Loon", "Macaw", "Magpie", "Mallard", "Mockingbird", "Mynah", "Nighthawk", "Nuthatch", "Oriole", "Osprey",
+  "Ostrich", "Owl", "Parakeet", "Parrot", "Partridge", "Peacock", "Pelican", "Penguin", "Pigeon", "Plover",
+  "Puffin", "Quail", "Raven", "Robin", "Roadrunner", "Rook", "Sandpiper", "Seagull", "Shrike", "Skua",
+  "Snipe", "Sparrow", "Starling", "Stilt", "Stork", "Swallow", "Swan", "Swift", "Tanager", "Tern",
+  "Thrush", "Titmouse", "Toucan", "Turkey", "Vireo", "Vulture", "Warbler", "Waxwing", "Weaver", "Wigeon",
+  "Woodcock", "Woodpecker", "Wren", "Anhinga", "BeeEater", "Bittern", "Blackbird", "Bobolink", "Bulbul", "Bunting",
+  "BushTit", "Chough", "Coot", "Creeper", "Curlew", "Darter", "Dotterel", "Drongo", "Dunnock", "Eider",
+  "Fieldfare", "Flycatcher", "Gannet", "Godwit", "Grackle", "Greylag", "Grouse", "Guillemot", "Harrier", "Heron",
+  "Hoatzin", "Iora", "Jacana", "Kea", "Killdeer", "Kingbird", "Kittiwake", "Limpkin", "Lorikeet", "Lyrebird",
+  "Manakin", "Murre", "Nightjar", "Ortolan", "Ovenbird", "Peafowl", "Phalarope", "Pipit", "Quetzal", "Rail",
+  "Redpoll", "Roller", "Rosella", "Shearwater", "Skimmer", "Spoonbill", "Stint", "Sunbird", "Tattler", "Teal",
+  "Thrasher", "Trogon", "Tyrant", "Wagtail", "Wheatear", "Whinchat", "Willet", "Woodstar", "Yuhina", "Swiftlet",
+  "Finch", "Robin", "Swan", "Duck", "Loon", "Jay", "Eagle", "Owl", "Tern", "Gull",
+  "Wren", "Crow", "Puffin", "Kite", "Heron", "Falcon", "Raven", "Ibis", "Dove", "Goose",
+  "Magpie", "Pelican", "Peacock", "Turkey", "Pigeon", "Canary", "Parrot", "Crane", "Cuckoo", "Stork",
+  "Osprey", "Parakeet", "Toucan", "Swallow", "Stilt", "Grebe", "Bunting", "Kestrel", "Hawk", "Vulture",
+  "Warbler", "Waxwing", "Oriole", "Mockingbird", "Sparrow", "Cardinal", "Bluebird", "Woodpecker", "Goldfinch", "Mallard"
+];
+const maxWrongGuesses = 6;
+let wrongGuesses = 0;
+let guessedLetters = [];
+let word = "";
+
+const wordDisplay = document.getElementById("wordDisplay");
+const letterButtons = document.getElementById("letterButtons");
+const guessesLeftDisplay = document.getElementById("guessesLeft");
+const message = document.getElementById("message");
+
+// Pick random word from wordList
+function chooseWord() {
+  word = wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+}
+
+// Create blanks / bird cages
+function displayWord() {
+  wordDisplay.innerHTML = "";
+  for (let letter of word) {
+    const letterBox = document.createElement("div");
+    letterBox.classList.add("letterBox");
+
+    if (guessedLetters.includes(letter)) {
+      letterBox.innerHTML = "<div class='birdEmoji'>🐦</div>"
+      letterBox.innerHTML += `<div class="revealedLetter center">${letter}</div>`;
+      letterBox.classList.add("free");
+    } else {
+      letterBox.innerHTML = "𐂺"
+      letterBox.classList.add("caged");
+    }
+
+    wordDisplay.appendChild(letterBox);
+  }
+}
+
+// Create A-Z buttons
+function createLetterButtons() {
+  letterButtons.innerHTML = "";
+  for (let i = 97; i <= 122; i++) {
+    const letter = String.fromCharCode(i);
+    const button = document.createElement("button");
+    button.textContent = letter;
+    button.classList.add("letterButton");
+    button.addEventListener("click", () => handleGuess(letter, button));
+    letterButtons.appendChild(button);
+  }
+}
+
+// Handle letter guessing
+function handleGuess(letter, button) {
+  button.disabled = true;
+
+  if (word.includes(letter)) {
+    guessedLetters.push(letter);
+    displayWord();
+    checkWin();
+  } else {
+    wrongGuesses++;
+    guessesLeftDisplay.textContent = maxWrongGuesses - wrongGuesses;
+    checkLose();
+  }
+}
+
+// Check for win condition
+function checkWin() {
+  const wordLetters = [...new Set(word)];
+  const guessedSet = new Set(guessedLetters);
+
+  if (wordLetters.every(l => guessedSet.has(l))) {
+    message.innerHTML = `Yippie! All the birds have been freed!<br>You win!
+    <br><br><span id='reloadWin'>Reload Tab to restart :)</span>`;
+    disableAllButtons();
+  }
+}
+
+// Check for lose condition
+function checkLose() {
+  if (wrongGuesses >= maxWrongGuesses) {
+    message.innerHTML = `Uh oh, you ran out of guesses! The birds remain trapped...
+      <br>Your word was <span id="birdAnswer"><strong>${word}</strong></span>.
+      <br><br><span id='reloadLose'>Reload Tab to restart :(</span>`;
+    disableAllButtons();
+  }
+}
+
+// Disable buttons when game is over
+function disableAllButtons() {
+  const allButtons = letterButtons.querySelectorAll("button");
+  allButtons.forEach(button => button.disabled = true);
+}
+
+// To start game
+function initGame() {
+  chooseWord();
+  guessedLetters = [];
+  wrongGuesses = 0;
+  guessesLeftDisplay.textContent = maxWrongGuesses;
+  message.textContent = "";
+  displayWord();
+  createLetterButtons();
+}
+
+// Actually start the game
+initGame();
--- readMe.txt
+++ readMe.txt
@@ -1,22 +1,98 @@
-This is the homepage for my bird-lover puzzle games!
+Welcome to the collection of my three bird web games!
 
-"Puzzles"
---> This is a jigsaw puzzle game where you can choose which image you want (from a selection)
-    and it'll be split into a grid where you then have to drag and drop the pieces
-    to put it back together in the way that it originally was.
 
-"Word Search"
---> Like any other word search, there will be a grid of letters that have words hidden
-    in-between, and you'll have to find the words from the given list of bird-related words.
+*** "Puzzle Perch" (jigsaw puzzles) ***
 
-"Birdcage"
---> This is my fun little spin on the classic 'Hangman', but instead of hanging a stick figure,
-    you'll have to guess the letters correctly to set the birds free from their cages.
-    If you run out of guesses, the birds will stay locked in the cages forever!
-    --or not if you want to try again. :P
+How to Play:
+>   Start by choosing a photo from the selection shown above the puzzle board.
+    Then you may choose how many pieces you want, ranging from 3x3 (very easy) to 7x7 (very challenging).
+    The cut-up image will appear--in pieces--in the top left corner.
+    You must drag each piece to its correct position to create a complete image.
+    Basic directions can be found below the puzzle board.
 
-any input or feedback is greatly appreciated!
+Current Issues:
+>   The script doesn't check if the completed picture is correct,
+    so you have to trust your judgement when making the puzzle(s).
+
+Potential Bug Fixes:
+>   Add a system that checks if the image is correct, and if it is, alert the player
+    and let them either reload the tab or choose another image to restart the game.
+
+Future Releases:
+>   I may add a feature that allows the user to import their own image
+    (and either crop it or resize it to fit the canvas).
+
+Copyright/Reproduction:
+>   Fonts are from Google Fonts.
+>   Images are from Pixabay.
+>   Please do not reproduce any part of Puzzle Perch without my permission.
+
+
+
+*** "Words of a Feather" (word search) ***
+How to Play:
+>   A list of 6 random, bird-related words will be chosen from a premade list.
+    You must find those six words placed [horizontally] throughout the 10x10 grid.
+    Once you locate a word in the grid, click it and enter the word!
+    Upon finding all the words, you will be prompted to reload the tab to restart the game.
+    There is no time or guess limit, so keep searching until all the words have been found!
+    Basic directions can be found below the Word List.
+
+Current Issues:
+>   The system of play relies on trusting that the user won't
+    simply enter each word in without looking through the grid.
+>   The words can only be placed horizontally,
+    which doesn't allow for much of a challenge.
+>   
+
+Potential Bug Fixes:
+>   Add a feature/system that makes it so the user must click on the word itself
+    before typing it in, or else it'll tell them there's no word where they clicked.
+    Also 
+>   Add a feature that allows words to be placed vertically as well
+    (and possibly diagonally, too).
+    
+Future Releases:
+>   I could add difficulty levels (which can affect the grid size, number of words, length of words. )
+>   (may be too complicated (for me)) Change the word finding system so that the user can
+    click and drag the word instead of typing it in.
+
+Copyright/Reproduction:
+>   Fonts are from Google Fonts
+>   Image(s) are from Pixabay
+>   Please do not reproduce any part of Words of a Feather without my permission.
+
+
+
+*** "Birdcage" (hangman) ***
+How to Play:
+>   This game is my take on hangman, but using birds trapped in cages instead of the classic stick figure.
+    It starts off with a random word selected from a premade list, and each blank has a cage on it.
+    To guess a letter, simply click one of the buttons displayed below the blanks.
+    With each letter guessed correctly, the spot(s) will be filled in with that letter and
+    a bird overhead, representing the bird being freed from its respective cage.
+    The remaining incorrect guesses display below the letter buttons, and each incorrect guess
+    decreases the number by one.
+    Basic directions can be found below the incorrect guess count/message box.
+
+Current Issues:
+>   The cages/birds could be reworked (visually).
+
+Potential Bug Fixes:
+>   Add an animation for the bird(s) flying out of the cage(s) when freed.
+
+Future Releases:
+>   I could add difficulty levels that would most likely just affect the number of guesses,
+    but it could also choose from a list of longer (and super short?) words.
+
+Copyright/Reproduction:
+>   Fonts are from Google Fonts
+>   Image(s) are from Pixabay
+>   Please do not reproduce any part of Birdcage without my permission.
+
+
+any input or feedback (particularly on bugs/issues) is greatly appreciated!
 contact me at cherka3@mail.nmc.edu  <3
-thank you for reading, and have a great day :)
+thank you for reading, I hope you enjoy my games, and have a great day! :)
 
-© Emily Cherkasova, 2025
+© Emily Cherkasova  March 7, 2025
